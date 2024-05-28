fd24f657abbe44438959a62d199e1d9604d65644
Shahriyor <shahriyoradhamov@icloud.com>
2024-05-28 22:30:19 +0500

    added some design to the product

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,62 @@
-const App = () => {
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Product from './components/Product';
+
+
+function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>
+              &#9776;
+        </button>
+            <Link to="/" >amazona</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <ul>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
+
+            <li>
+              <a href="index.html">Shirts</a>
+            </li>
+
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Routes>
+              <Route path="/product/:id" element={<Product/>} />
+              <Route path="/" element={<Home/>}/>
+            </Routes>
+
+
+          </div>
+
+        </main>
+        <footer className="footer">
+          All right reserved.
+    </footer>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
